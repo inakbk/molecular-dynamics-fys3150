a7767706c2af40af5661ee843db1daf1f0789e06
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-12-05 15:13:07 +0100

    implement L-J pot

--- potentials/lennardjones.cpp
+++ potentials/lennardjones.cpp
@@ -11,4 +11,19 @@ void LennardJones::calculateForces(System *system)
 {
     m_potentialEnergy = 0; // Remember to compute this in the loop
 
+    x_ij = ;
+    y_ij = ;
+    z_ij = ;
+
+    r_ij = ;
+
+    r = sigma/r_ij;
+    r6 = r*r*r*r*r*r;
+    r8 = r6*r*r;
+    some_numbers = ( 24*m_epsilon/(m_sigma*m_sigma) )*r8*( 2*r6 - 1);
+
+    m_force_x = some_numbers*x_ij;
+    m_force_y = some_numbers*y_ij;
+    m_force_y = some_numbers*z_ij;
+
 }
--- potentials/lennardjones.h
+++ potentials/lennardjones.h
@@ -7,6 +7,11 @@ class LennardJones : public Potential
 private:
     double m_sigma = 1.0;
     double m_epsilon = 1.0;
+
+    double m_force_x = 0;
+    double m_force_y = 0;
+    double m_force_z = 0;
+
 public:
     LennardJones(double sigma, double epsilon);
     virtual void calculateForces(System *system);
