07725f8ca42264bdcbbfb7a98b42ab4c1aa007b7
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-12-05 19:36:32 +0100

    implementing velocity verlet

--- integrators/velocityverlet.cpp
+++ integrators/velocityverlet.cpp
@@ -4,5 +4,14 @@
 
 void VelocityVerlet::integrate(System *system, double dt)
 {
+    system->calculateForces();
+    for(Atom *atom : system->atoms()) {
+        vec3 velocity_half = atom->velocity + atom->force*0.5*dt / atom->mass();
+        atom->position += velocity_half*dt;
+        vec3 force_next =
+        atom->velocity = velocity_half + force_next*0.5*dt / atom->mass();
 
+    }
+
+    system->applyPeriodicBoundaryConditions();
 }
