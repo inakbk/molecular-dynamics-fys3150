f16a887c0df246589f29be2a9b41848d84aa5546
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-12-08 18:37:45 +0100

    indexes fixed in python read_file

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int numberOfArguments, char **argumentList)
     // If a fifth argument is provided, it is the integrator number
     if(numberOfArguments > 5) integratorNumber = atoi(argumentList[5]);
     // If a sixth argument is provided, it is the number of timesteps
-    if(numberOfArguments > 6) integratorNumber = atoi(argumentList[6]);
+    if(numberOfArguments > 6) numberOfTimesteps = atoi(argumentList[6]);
 
     /*
     cout << "One unit of length is " << UnitConverter::lengthToSI(1.0) << " meters" << endl;
--- run_plot.py
+++ run_plot.py
@@ -11,26 +11,25 @@ def read_file(filename):
     all_lines = infile.readlines()
     infile.close()
 
-    time = zeros(len(all_lines))
-    instantTemperature = zeros(len(all_lines))
-    kineticEnergy = zeros(len(all_lines))
-    potentialEnergy = zeros(len(all_lines))
-    totalEnergy = zeros(len(all_lines))
-    diffusionConstant = zeros(len(all_lines))
-    meanSquareDisplacement = zeros(len(all_lines))
+    time = zeros(len(all_lines)-2)
+    instantTemperature = zeros(len(all_lines)-2)
+    kineticEnergy = zeros(len(all_lines)-2)
+    potentialEnergy = zeros(len(all_lines)-2)
+    totalEnergy = zeros(len(all_lines)-2)
+    diffusionConstant = zeros(len(all_lines)-2)
+    meanSquareDisplacement = zeros(len(all_lines)-2)
 
-    i = 1
-    while i < len(all_lines) - 1:
-        time[i] = float(all_lines[i].split()[1])
-        instantTemperature[i] = float(all_lines[i].split()[2])
-        kineticEnergy[i] = float(all_lines[i].split()[3])
-        potentialEnergy[i] = float(all_lines[i].split()[4])
-    	totalEnergy[i] = float(all_lines[i].split()[5])
-        diffusionConstant[i] = float(all_lines[i].split()[6])
-        meanSquareDisplacement[i] = float(all_lines[i].split()[7])
+    i = 0
+    while i < len(all_lines) - 2:
+        time[i] = float(all_lines[i+1].split()[1])
+        instantTemperature[i] = float(all_lines[i+1].split()[2])
+        kineticEnergy[i] = float(all_lines[i+1].split()[3])
+        potentialEnergy[i] = float(all_lines[i+1].split()[4])
+    	totalEnergy[i] = float(all_lines[i+1].split()[5])
+        diffusionConstant[i] = float(all_lines[i+1].split()[6])
+        meanSquareDisplacement[i] = float(all_lines[i+1].split()[7])
 
     	i += 1
-
     return time, instantTemperature, kineticEnergy, potentialEnergy, totalEnergy, diffusionConstant, meanSquareDisplacement
 
 
@@ -39,11 +38,11 @@ def read_file_E(filename):
     all_lines = infile.readlines()
     infile.close()
 
-    totalEnergy = zeros(len(all_lines))
+    totalEnergy = zeros(len(all_lines)-2)
 
-    i = 1
-    while i < len(all_lines) - 1:
-        totalEnergy[i] = float(all_lines[i].split()[5])
+    i = 0
+    while i < len(all_lines) - 2:
+        totalEnergy[i] = float(all_lines[i+1].split()[5])
 
         i += 1
 
@@ -62,8 +61,6 @@ latticeConstant = 5.26 # measured in angstroms
 
 length_of_list = 20
 dt_list = linspace(0.0001, 0.1, length_of_list)
-
-
 
 #compiling once:
 os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
@@ -106,10 +103,10 @@ dt = 0.0526 # Measured MD units, is about the same as 5e-15 seconds
 integratorNumber = 2 # initially set to Velocity Verlet, 1 is Euler-Cromer
 
 #compiling once:
-os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
+#os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
 
 #running cpp code
-os.system('./main %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, integratorNumber))
+#os.system('./main %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, integratorNumber))
 
 time, instantTemperature, kineticEnergy, potentialEnergy, totalEnergy, diffusionConstant, meanSquareDisplacement = read_file('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt*10000), integratorNumber))
 
@@ -118,8 +115,7 @@ plot(time, meanSquareDisplacement)
 xlabel('time [MD units]', fontsize=18)
 ylabel(r'$\langle r^2(t) \rangle$ [MD units]', fontsize=18)
 legend(['Velocity Verlet'], fontsize=16, loc='lower left')
-title('numberOfUnitCells= %s, initialTemperature= %s, latticeConstant= %s, time=' %(numberOfUnitCells, initialTemperature, latticeConstant), fontsize=16)
-
+title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s, time= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), time[-1]), fontsize=16)
 
 show()
 
