885890bdfbd0e40d6838bc748237053c0b8e6f8b
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-12-08 19:36:15 +0100

    sigma_E plot looks correct

--- run_plot.py
+++ run_plot.py
@@ -53,15 +53,14 @@ def read_file_E(filename):
 """
 #plotting sigma_E vs dt:
 
-"""
 #all in MD units!
 numberOfUnitCells = 5
 initialTemperature = 2.5 # measured in Kelvin
 latticeConstant = 5.26 # measured in angstroms
 
 length_of_list = 20
-dt_list = linspace(0.0001, 0.1, length_of_list)
-
+dt_list = linspace(0.005, 0.1, length_of_list)
+"""
 #compiling once:
 os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
 
@@ -69,14 +68,14 @@ os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -
 for dt in dt_list:
 	os.system('./main %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, 1))
 	os.system('./main %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, 2))
-  
+"""
 sigmaEnergyEuler = zeros(length_of_list)
 sigmaEnergyVV = zeros(length_of_list)
 
 for i in range(length_of_list):
-    totalEnergy = read_file_E('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt_list[i]*10000), 1))
+    totalEnergy = read_file_E('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(round(dt_list[i]*10000)), 1))
     sigmaEnergyEuler[i] = sqrt(sum(totalEnergy**2)/len(totalEnergy) - (sum(totalEnergy)/len(totalEnergy))**2)
-    totalEnergy = read_file_E('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt_list[i]*10000), 2))
+    totalEnergy = read_file_E('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(round(dt_list[i]*10000)), 2))
     sigmaEnergyVV[i] = sqrt(sum(totalEnergy**2)/len(totalEnergy) - (sum(totalEnergy)/len(totalEnergy))**2)
 
 figure(1)
@@ -85,15 +84,14 @@ hold('on')
 plot(dt_list, sigmaEnergyVV, 'r-o')
 xlabel('dt [MD units]', fontsize=18)
 ylabel('$\sigma_E$ [MD units]', fontsize=18)
-legend(['Euler', 'Velocity Verlet'], fontsize=16, loc='lower left')
-title('numberOfUnitCells= %s, initialTemperature= %s, latticeConstant= %s, time=' %(numberOfUnitCells, initialTemperature, latticeConstant), fontsize=16)
-"""
+legend(['Euler', 'Velocity Verlet'], fontsize=16, loc='upper left')
+title('numberOfUnitCells= %s, initialTemperature= %s\n latticeConstant= %s, numberOfTimesteps= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(totalEnergy)), fontsize=16)
 
 """
 ------------------------------------------------------------------------------------------
 """ 
 #plotting statistics for one dt
-
+"""
 #all in MD units!
 numberOfUnitCells = 15
 initialTemperature = 2.5 # measured in MD approx 2.5=300 Kelvin
@@ -101,12 +99,13 @@ latticeConstant = 5.26 # measured in angstroms (MD)
 
 dt = 0.0526 # Measured MD units, is about the same as 5e-15 seconds
 integratorNumber = 2 # initially set to Velocity Verlet, 1 is Euler-Cromer
+numberOfTimesteps = 1000
 
 #compiling once:
 #os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
 
 #running cpp code
-#os.system('./main %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, integratorNumber))
+#os.system('./main %s %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, integratorNumber, numberOfTimesteps))
 
 time, instantTemperature, kineticEnergy, potentialEnergy, totalEnergy, diffusionConstant, meanSquareDisplacement = read_file('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt*10000), integratorNumber))
 
@@ -116,6 +115,6 @@ xlabel('time [MD units]', fontsize=18)
 ylabel(r'$\langle r^2(t) \rangle$ [MD units]', fontsize=18)
 legend(['Velocity Verlet'], fontsize=16, loc='lower left')
 title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s, time= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), time[-1]), fontsize=16)
-
+"""
 show()
 
