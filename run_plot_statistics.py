f3eabd660e4c17b150dbd6dfc66e56d5f9badfcc
Ina Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2015-12-09 14:09:46 +0100

    testing

--- run_plot_statistics.py
+++ run_plot_statistics.py
@@ -55,13 +55,13 @@ def read_file_E(filename):
 #plotting statistics vs time for one dt
 
 #all in MD units!
-numberOfUnitCells = 10
-initialTemperature = 16.7 # measured in MD approx 2.5=300 Kelvin, 8.35 = 1000 Kelvin, 16.7 = 2000 Kelvin
+numberOfUnitCells = 5
+initialTemperature = 2.5 # measured in MD approx 2.5=300 Kelvin, 8.35 = 1000 Kelvin, 16.7 = 2000 Kelvin
 latticeConstant = 5.26 # measured in angstroms (MD)
 
 dt = 0.05 # Measured MD units, is about the same as 5e-15 seconds
 integratorNumber = 2 # initially set to Velocity Verlet, 1 is Euler-Cromer
-numberOfTimesteps = 1000
+numberOfTimesteps = 10000
 
 #compiling once:
 #os.system('g++ -o main *.cpp math/*.cpp potentials/*.cpp integrators/*.cpp -I. -O3 -std=c++11')
@@ -70,7 +70,9 @@ numberOfTimesteps = 1000
 #os.system('./main %s %s %s %s %s %s' %(numberOfUnitCells, initialTemperature, latticeConstant, dt, integratorNumber, numberOfTimesteps))
 
 time, instantTemperature, kineticEnergy, potentialEnergy, totalEnergy, diffusionConstant, meanSquareDisplacement = read_file('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt*10000), integratorNumber))
+#time, instantTemperature, kineticEnergy, potentialEnergy, totalEnergy, diffusionConstant, meanSquareDisplacement = read_file('run_plot_python_output/statistics_file_NrOfCells%s_T%s_b%s_dt%s_int%s.txt' %(numberOfUnitCells, int(initialTemperature*1000), int(latticeConstant*1000), int(dt*10000), integratorNumber))
 
+print len(totalEnergy)
 
 figure(1)
 plot(time, instantTemperature)
@@ -81,19 +83,20 @@ title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, num
 
 figure(2)
 plot(time, kineticEnergy)
-xlabel('time [MD units]', fontsize=18)
-ylabel(r'$\langle E_k \rangle$ [MD units]', fontsize=18)
-legend(['Velocity Verlet'], fontsize=16, loc='lower left')
-title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s,dt= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), dt), fontsize=16)
+hold('on')
+#xlabel('time [MD units]', fontsize=18)
+#ylabel(r'$\langle E_k \rangle$ [MD units]', fontsize=18)
+#legend(['Velocity Verlet'], fontsize=16, loc='lower left')
+#title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s,dt= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), dt), fontsize=16)
 
-figure(3)
+#figure(3)
 plot(time, potentialEnergy)
-xlabel('time [MD units]', fontsize=18)
-ylabel('$V$ [MD units]', fontsize=18)
-legend(['Velocity Verlet'], fontsize=16, loc='lower left')
-title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s,dt= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), dt), fontsize=16)
+#xlabel('time [MD units]', fontsize=18)
+#ylabel('$V$ [MD units]', fontsize=18)
+#legend(['Velocity Verlet'], fontsize=16, loc='lower left')
+#title('numberOfUnitCells= %s, initialTemperature= %s \n latticeConstant= %s, numberOfTimesteps= %s,dt= %s' %(numberOfUnitCells, initialTemperature, latticeConstant, len(time), dt), fontsize=16)
 
-figure(4)
+#figure(4)
 plot(time, totalEnergy)
 xlabel('time [MD units]', fontsize=18)
 ylabel('$E$ [MD units]', fontsize=18)
